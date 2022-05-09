113c004883b5cbe7d4caf947af7ea0c5e9617aa5
ThuryaNasser <63455773+ThuryaNasser@users.noreply.github.com>
2022-05-09 16:25:30 +0300

    add type orm

--- src/teas/entities/teas.entities.ts
+++ src/teas/entities/teas.entities.ts
@@ -1,6 +1,15 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity() // sql table === 'tea'
 export class Tea {
+  @PrimaryGeneratedColumn() //auto increment by default
   id: number;
+
+  @Column() // required by default
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
 }
