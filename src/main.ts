8008c47dd907343e784ed41cfcc8b6c0681cbcf6
ThuryaNasser <63455773+ThuryaNasser@users.noreply.github.com>
2022-07-17 23:26:08 +0300

    Understanding Binding Techniques

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,27 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /*
+
+  Nest building blocks : 
+  -Exception filters -> handling unhandled error thats may happens ( control specific response )  
+  -Pipes -> to handel transformation and validation 
+  -Guards -> to determine if a given request need a certain condition like authentications 
+  -Interceptors:
+     -> to bind extra logic before or after methods execution 
+     -> to transform the result returned from a method 
+     -> to extend basic methods behaviors 
+     -> override a method
+
+
+  Nest building blocks can be:
+    Globally-scoped -> at app.module 
+    Controller-scoped, 
+    Method-scoped, 
+    Param-scoped (Pipes only).
+    
+    */
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, //Enabling "whitelist" feature of ValidationPipe
