e221e1212cbb552045a9133fcc9daa3da3c8736e
ThuryaNasser <63455773+ThuryaNasser@users.noreply.github.com>
2022-07-18 00:53:37 +0300

    Introducing the Config Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { AppController } from './app.controller';
@@ -7,14 +8,16 @@ import { TeasModule } from './teas/teas.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TeasModule,
     TypeOrmModule.forRoot({
+      synchronize: true,
       type: 'postgres', // type of our database
-      host: 'localhost', // database host
-      port: 35432, // database host
-      username: 'postgres', // username
-      password: 'postgres', // user password
-      database: 'postgres', // name of our database,
+      host: process.env.DATABASE_HOST, // database host
+      port: +process.env.DATABASE_PORT, // database host
+      username: process.env.DATABASE_USER, // username
+      password: process.env.DATABASE_PASSWORD, // user password
+      database: process.env.DATABASE_NAME, // name of our database,
       entities: [join(__dirname, '/**/*.entity{.ts,.js}')],
       autoLoadEntities: true, // models will be loaded automatically
       // synchronize: true, // your entities will be synced with the database(recommended: disable in production)
