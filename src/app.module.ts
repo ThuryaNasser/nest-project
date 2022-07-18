7d014c703bb972c9573a5f540459cf5c36452392
ThuryaNasser <63455773+ThuryaNasser@users.noreply.github.com>
2022-07-18 03:53:56 +0300

    Asynchronously Configure Dynamic Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,21 +8,24 @@ import { TeasModule } from './teas/teas.module';
 
 @Module({
   imports: [
+    // forRootAsync is loaded after every thing is loaded
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres', // type of our database
+        host: process.env.DATABASE_HOST, // database host
+        port: parseInt(process.env.DATABASE_PORT), // database host
+        username: process.env.DATABASE_USER, // username
+        password: process.env.DATABASE_PASSWORD, // user password
+        database: process.env.DATABASE_NAME, // name of our database,
+        dropSchema: false,
+        migrationsRun: false,
+        synchronize: false, // your entities will be synced with the database(recommended: disable in production)
+        entities: [join(__dirname, '/**/*.entity{.ts,.js}')],
+        autoLoadEntities: true, // models will be loaded automatically
+      }),
+    }),
     ConfigModule.forRoot(),
     TeasModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres', // type of our database
-      host: process.env.DATABASE_HOST, // database host
-      port: parseInt(process.env.DATABASE_PORT), // database host
-      username: process.env.DATABASE_USER, // username
-      password: process.env.DATABASE_PASSWORD, // user password
-      database: process.env.DATABASE_NAME, // name of our database,
-      dropSchema: false,
-      migrationsRun: false,
-      synchronize: false, // your entities will be synced with the database(recommended: disable in production)
-      entities: [join(__dirname, '/**/*.entity{.ts,.js}')],
-      autoLoadEntities: true, // models will be loaded automatically
-    }),
   ],
   controllers: [AppController],
   providers: [AppService],
