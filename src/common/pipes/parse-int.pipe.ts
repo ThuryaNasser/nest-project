7762f15d8950958376e50e5fa168edec50a86cf0
ThuryaNasser <63455773+ThuryaNasser@users.noreply.github.com>
2022-07-21 04:46:29 +0300

    Creating Custom Pipes

---
+++ src/common/pipes/parse-int.pipe.ts
@@ -0,0 +1,22 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+
+@Injectable()
+export class ParseIntPipe implements PipeTransform {
+  //value:the input value of the currently processed argument
+  // before it is received by our route handling method
+  // metadata:the currently processed argument
+  transform(value: string, metadata: ArgumentMetadata) {
+    const val = parseInt(value, 10);
+    if (isNaN(val)) {
+      throw new BadRequestException(
+        `Validation failed. "${val}" is not an integer.`,
+      );
+    }
+    return val;
+  }
+}
